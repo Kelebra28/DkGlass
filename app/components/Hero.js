d40cb770242a9321310b79ae3a314c8441bd9efe
angeloluna <angel.luna.najera@gmail.com>
2023-06-01 14:33:11 -0600

    [update] hero carousel desktop

--- app/components/Hero.js
+++ app/components/Hero.js
@@ -1,5 +1,29 @@
+'use client'
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 import Image from "next/image"
 import Button from "./common/Button"
+import CarouselCard from "./common/CarouselCard";
+
+const responsive = {
+  superLargeDesktop: {
+    // the naming can be any, depends on you.
+    breakpoint: { max: 4000, min: 3000 },
+    items: 5
+  },
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 3
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 2
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1
+  }
+};
 
 const Hero = () => (
   <div>
@@ -17,12 +41,59 @@ const Hero = () => (
 
     <div className="flex">
       <div className="">
-        <div className="relative w-screen h-[360px] hidden lg:flex">
-          <Image
-            src="/hero-desktop.png"
-            alt="Portada"
-            layout="fill"
-          />
+        <div className=" relative w-screen hidden lg:block pr-[20px]">
+          <Carousel responsive={responsive}>
+            <div className="w-[736px] h-[450px] relative">
+              <Image
+                src="/hero.png"
+                alt="Portada"
+                layout="fill"
+              />
+            </div>
+            <div className="w-[736px] h-[450px] relative">
+              <Image
+                src="/hero.png"
+                alt="Portada"
+                layout="fill"
+              />
+            </div>
+            <div className="w-[736px] h-[450px] relative">
+              <Image
+                src="/hero.png"
+                alt="Portada"
+                layout="fill"
+              />
+            </div>
+            <div className="w-[736px] h-[450px] relative">
+              <Image
+                src="/hero.png"
+                alt="Portada"
+                layout="fill"
+              />
+            </div>
+            <div className="w-[736px] h-[450px] relative">
+              <Image
+                src="/hero.png"
+                alt="Portada"
+                layout="fill"
+              />
+            </div>
+            <div className="w-[736px] h-[450px] relative">
+              <Image
+                src="/hero.png"
+                alt="Portada"
+                layout="fill"
+              />
+            </div>
+            <div className="w-[736px] h-[450px] relative">
+              <Image
+                src="/hero.png"
+                alt="Portada"
+                layout="fill"
+              />
+            </div>
+
+          </Carousel>
         </div>
         <div className="px-[49px] lg:px-[82px] lg:mt-[20px]">
           <p className="mt-[19px] text-lg font-semibold lg:text-[28px]">Encuentra soluciones creativas y atractivas para tus necesidades de decoración con nuestros accesorios de vidrio para interiores.</p>
@@ -36,7 +107,13 @@ const Hero = () => (
               <Button text="Regístrate" />
             </div>
           </div>
-          <div className="w-4/12 border flex justify-center items-center">Video</div>
+          <div className="w-4/12 flex justify-center items-center relative">
+            <Image
+              src="/hero.png"
+              alt="Portada"
+              layout="fill"
+            />
+          </div>
           <div className="w-4/12 flex justify-center items-center relative flex-col">
             <Button text="Contactanos" background="blue" border="none" color="white" />
             <div className="flex mt-[50px]">
@@ -68,51 +145,50 @@ const Hero = () => (
       </div>
       <div className="w-[68px] box-border bg-[#1081A9] z-20 h-[1024px] absolute top-0 right-0 hidden lg:flex flex-col justify-between">
         <div className="pt-[100px] pl-[15px]">
-          <div className="w-[26px] h-[26px] my-[10px] relative">
+          <div className="w-[26px] h-[26px] my-[20px] relative">
             <Image
               src="/icons/facebook-white.svg"
               alt="Portada"
               layout="fill"
             />
           </div>
-          <div className="w-[26px] h-[26px] my-[10px] relative">
+          <div className="w-[26px] h-[26px] my-[20px] relative">
             <Image
-              src="/icons/facebook-white.svg"
+              src="/icons/Linkedin-white.svg"
               alt="Portada"
               layout="fill"
             />
           </div>
-          <div className="w-[26px] h-[26px] my-[10px] relative">
+          <div className="w-[26px] h-[26px] my-[20px] relative">
             <Image
-              src="/icons/facebook-white.svg"
+              src="/icons/twiter-white.svg"
               alt="Portada"
               layout="fill"
             />
           </div>
-          <div className="w-[26px] h-[26px] my-[10px] relative">
+          <div className="w-[26px] h-[26px] my-[20px] relative">
             <Image
-              src="/icons/facebook-white.svg"
+              src="/icons/instagram-white.svg"
               alt="Portada"
               layout="fill"
             />
           </div>
-          <div className="w-[26px] h-[26px] my-[10px] relative">
+          <div className="w-[26px] h-[26px] my-[20px] relative">
             <Image
-              src="/icons/facebook-white.svg"
-              alt="Portada"
-              layout="fill"
-            />
-          </div>
-          <div className="w-[26px] h-[26px] my-[10px] relative">
-            <Image
-              src="/icons/facebook-white.svg"
+              src="/icons/youtube-white.svg"
               alt="Portada"
               layout="fill"
             />
           </div>
         </div>
-        <div className="bg-dk-secondary mb-[250px] w-[67px] h-[67px] rounded-lg">
-
+        <div className="bg-dk-secondary mb-[250px] w-[67px] h-[67px] rounded-lg relative flex justify-center items-center">
+          <div className="w-[40px] h-[40px] relative">
+            <Image
+              src="/icons/message-white.svg"
+              alt="Portada"
+              layout="fill"
+            />
+          </div>
         </div>
       </div>
     </div>
