e8f0d77576aa684d5dce61796e45a0f651b4bf49
angeloluna <angel.luna.najera@gmail.com>
2023-06-15 20:24:14 -0600

    [add] new page

--- app/components/common/SideBar.js
+++ app/components/common/SideBar.js
@@ -1,9 +1,10 @@
 import Image from "next/image"
+import Link from "next/link"
 const SideBar = ({ handleClickMenu }) => (
   <div className="fixed top-0 left-0 right-0 bottom-0 z-50">
-    <div className="bg-[white] absolute top-0 bottom-0 left-0 right-[70%]">
+    <div className="bg-[white] absolute top-0 bottom-0 left-0 lg:right-[70%] right-[20%]">
       <header className="bg-dk-main relative flex flex-col items-end pb-[54px] border-b-dk-secondary border-b-[10px] pt-[12px] pr-[10px]">
-        <div onClick={handleClickMenu} className="w-[22px] h-[22px] relative">
+        <div onClick={handleClickMenu} className="w-[22px] h-[22px] relative cursor-pointer">
           <Image
             src="/icons/close-white.svg"
             alt="Logo"
@@ -20,7 +21,7 @@ const SideBar = ({ handleClickMenu }) => (
           </div>
         </div>
       </header>
-      <div className="absolute top-[160px] bottom-0 left-0 right-0 pl-[40px] pr-[30px] overflow-scroll">
+      <div className="absolute top-[160px] bottom-0 left-0 right-0 pl-[40px] pr-[30px] overflow-scroll pb-[50px]">
         <section className="border-b-[2px] border-b-dk-secondary pt-[20px] pb-[20px] flex flex-col">
           <h2 className="text-[18px] font-medium hover:bg-dk-secondary pl-[12px] hover:text-[white]">Home</h2>
           <span className="mt-[15px] text-[12px] hover:bg-dk-secondary pl-[12px] hover:text-[white]">Sobre nosotros</span>
@@ -43,31 +44,34 @@ const SideBar = ({ handleClickMenu }) => (
           <span className="mt-[15px] text-[12px] hover:bg-dk-secondary pl-[12px] hover:text-[white]">Titulo</span>
         </section>
         <div className="flex flex-col mt-[87px]">
-          <div className="flex items-center">
-            <div className="w-[16px] h-[16px] relative">
-              <Image
-                src="/icons/phone.svg"
-                alt="Portada"
-                layout="fill"
-              />
+          <Link href="tel:+52 55 1204 0419">
+            <div className="flex items-center">
+              <div className="w-[16px] h-[16px] relative">
+                <Image
+                  src="/icons/phone.svg"
+                  alt="Portada"
+                  layout="fill"
+                />
+              </div>
+              <span className="text-[12px] font-semibold ml-[10px]">+52 55 1204 0419</span>
             </div>
-            <span className="text-[12px] font-semibold ml-[10px]">+52 55 1204 0419</span>
-
-          </div>
-          <div className="flex items-center mt-[12px]">
-            <div className="w-[23px] h-[23px] relative">
-              <Image
-                src="/icons/email.svg"
-                alt="Portada"
-                layout="fill"
-              />
+          </Link>
+          <Link href={`mailto:info@dkglass&velazco.com`}>
+            <div className="flex items-center mt-[12px]">
+              <div className="w-[23px] h-[23px] relative">
+                <Image
+                  src="/icons/email.svg"
+                  alt="Portada"
+                  layout="fill"
+                />
+              </div>
+              <span className="text-[12px] font-semibold ml-[10px]">info@dkglass&velazco.com</span>
             </div>
-            <span className="text-[12px] font-semibold ml-[10px]">+52 55 1204 0419</span>
-          </div>
+          </Link>
         </div>
       </div>
     </div>
-    <div onClick={handleClickMenu} className="bg-[black] opacity-30 absolute top-0 bottom-0 left-[30%] right-0" />
+    <div onClick={handleClickMenu} className="bg-[black] opacity-30 absolute top-0 bottom-0 left-[80%] lg:left-[30%] right-0" />
 
     
   </div>
