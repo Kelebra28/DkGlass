0ee89af83ae44808697ee26b677035d8aa55fcf0
Kelebra <ricardokelebra@gmail.com>
2023-08-06 19:45:27 -0600

    feat: change text, animation icon

--- app/layout.js
+++ app/layout.js
@@ -6,6 +6,7 @@ const inter = Cinzel({ subsets: ['latin'], weight: "400"})
 export const metadata = {
   title: 'Dk Glass & Velasco',
   description: 'What you imagine in glass',
+  image: 'https://dk-glass.vercel.app/logo-full.svg',
 }
 
 export default function RootLayout({ children }) {
