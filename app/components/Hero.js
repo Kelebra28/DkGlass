2e3e5b903a3616662d1e3e5852cb1364db0303d5
Kelebra <ricardokelebra@gmail.com>
2023-08-15 19:50:45 -0600

    fix: fix email and text langue

--- app/components/Hero.js
+++ app/components/Hero.js
@@ -172,7 +172,7 @@ const Hero = ({ handleContactPopUp }) => (
           <div className="w-4/12 flex  justify-center items-center relative flex-col">
             <Button className="hover_contat" onClick={handleContactPopUp} text="Contact us" background="blue" border="2px border-[#243c5a]" color="white" />
             <div className="flex mt-[50px]">
-              <Link href="tel:+52 55 1204 0419">
+              <Link href="tel: (832) 740-4977">
                 <div className="flex items-center  cursor-pointer animation-contact">
                   <div className="w-[16px] h-[16px] relative b">
                     <span className="text-[#60DBC5]">
