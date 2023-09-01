06772de089a2ce91bf86a3eea41e00192e3b3831
EiichiM <matsuyoubi@gmail.com>
2023-09-01 02:17:15 -0600

    images configuration responsive

--- app/components/common/LazyImages.tsx
+++ app/components/common/LazyImages.tsx
@@ -1,17 +1,18 @@
 import Image from 'next/image';
 
 interface LazyImageProps {
-    src: string,
-    alt: string,
-    style?: object,
-    className?:string
+  src: string,
+  alt: string,
+  style?: object,
+  className?: string
 }
 
-const LazyImage = ({ src, alt, style, className } : LazyImageProps) => {
+const LazyImage = ({ src, alt, style, className }: LazyImageProps) => {
   return (
     <Image
       src={src}
       alt={alt}
+      sizes="(min-width: 1040px) 50vw, 540px"
       className={className}
       loading="lazy"
       style={style}
--- next.config.js
+++ next.config.js
@@ -1,23 +1,33 @@
 /** @type {import('next').NextConfig} */
-const withFonts = require('next-fonts');
+const withFonts = require("next-fonts");
 
 const nextConfig = {
-    async headers() {
-        return [
+  async headers() {
+    return [
+      {
+        source: "/favicon.png",
+        headers: [
           {
-            source: '/favicon.png',
-            headers: [
-              {
-                key: 'Cache-Control',
-                value: 'public, max-age=86400',
-              },
-            ],
+            key: "Cache-Control",
+            value: "public, max-age=86400",
           },
-        ];
+        ],
       },
-}
+    ];
+  },
+  images: {
+    remotePatters: [
+      {
+        protocol: "https",
+        hostname: "www.dkglassvelasco.com",
+        post: "",
+        pathname: "/public/**",
+      },
+    ],
+  },
+};
 
-module.exports = withFonts(nextConfig)
+module.exports = withFonts(nextConfig);
 
 // module.exports = {
 //     // ...otras configuraciones...
