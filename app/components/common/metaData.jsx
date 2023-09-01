28d1467561add4f61f9018b88d7626b72ebea76b
Kelebra <ricardokelebra@gmail.com>
2023-09-01 11:31:11 -0600

    fix seo Eiichi

--- app/components/common/metaData.jsx
+++ app/components/common/metaData.jsx
@@ -5,7 +5,7 @@ const OGMetaTags = () => {
     <Head>
       <meta property="og:title" content="Dk Glass & Velasco" />
       <meta property="og:description" content="What you imagine in glass 2.0" />
-      <meta property="og:image" content="/public/favicon.png" />
+      <meta property="og:image" content="/favicon.png" />
       <meta property="og:url" content="https://www.dkglassvelasco.com" />
       <meta property="og:type" content="website" />
       <link rel="preload" as="font" />
